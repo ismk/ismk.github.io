c0d5901fd5f9bf84854931587df0b3e1d115bc33
Ismail Kalimi <ismailkalimi@gmail.com>
2016-07-15 17:15:31 -0500

    fixed nightmode js

--- js/nightmode.js
+++ js/nightmode.js
@@ -3,10 +3,25 @@
 body = $("body")
 post_title = $(".post-teaser__title")
 logo = $(".logo")
+check_nightmode = localStorage.getItem("nightmode");
 
+if(check_nightmode == "true") {
+    body.addClass("dark");
+    post_title.addClass("dark");
+    logo.addClass("dark");
+}
 
 $(".lights").on("click", function() {
-    body.toggleClass("dark");
-    post_title.toggleClass("dark");
-    logo.toggleClass("dark");
+    if(body.hasClass("dark")) {
+        body.removeClass("dark");
+        post_title.removeClass("dark");
+        logo.removeClass("dark");
+        localStorage.setItem("nightmode", "false");
+    }
+    else {
+        body.addClass("dark");
+        post_title.addClass("dark");
+        logo.addClass("dark");
+        localStorage.setItem("nightmode", "true");
+    }
 });
